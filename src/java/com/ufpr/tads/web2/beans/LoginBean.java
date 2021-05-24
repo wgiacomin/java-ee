9c49a0f8abcb276fc3877361a872eefaab0ab08a
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2021-05-24 14:02:52 -0300

    Add | CadastroBean + LoginBean

---
+++ src/java/com/ufpr/tads/web2/beans/LoginBean.java
@@ -0,0 +1,42 @@
+package com.ufpr.tads.web2.beans;
+
+import java.io.Serializable;
+
+public class LoginBean implements Serializable {
+
+    private int id;
+    private String login;
+    private String senha;
+
+    public LoginBean(String login, String senha) {
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public LoginBean(int id, String login, String senha) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+}
